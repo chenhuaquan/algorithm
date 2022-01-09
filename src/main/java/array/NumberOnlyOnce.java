39b85e01bd18929a34414cad516aee3c02a60843
Hua-Quan Chen <87644334@qq.com>
2022-01-09 23:16:10 +0800

    number only one

---
+++ src/main/java/array/NumberOnlyOnce.java
@@ -0,0 +1,15 @@
+package array;
+
+public class NumberOnlyOnce {
+    
+    public static void main( String[] args )
+    {
+        System.out.println( "NumberOnlyOnce" );
+        int[] numbers = {1, 4, 3, 4, 2, 2, 3};
+        
+        for (int i = 1; i < numbers.length; i++) {
+            numbers[0] ^= numbers[i];
+        }
+        System.out.println(numbers[0]);
+    }
+}
